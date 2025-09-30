5bf6f42fcbea03bdf07539a546cbcf0ab7ca5feb
yumao <1810498116@qq.com>
2025-09-30 17:33:59 +0800

    员工分页查询代码

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -22,4 +24,5 @@ public interface EmployeeMapper {
     void insert(Employee employee);
 
 
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,9 +1,10 @@
 package com.sky.service;
 
-import com.sky.constant.StatusConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
 public interface EmployeeService {
 
@@ -17,4 +18,5 @@ public interface EmployeeService {
     void save(EmployeeDTO employee);
 
 
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
